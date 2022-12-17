032bff901343be3e2cc5cce8d3b6d432bd79c825
SekoVper <damehlivingstone1@gmail.com>
2022-12-17 15:18:40 +0100

    Refactor code to use Hooks

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import { React, useState } from 'react';
 import AddBook from './AddBook';
 import BooksList from './BooksList';
 
-const books = [
-  { id: 1, title: 'Name the Tree', author: 'Seko' },
-  { id: 2, title: 'How We became Men', author: 'SekoViper' },
-  { id: 3, title: 'In the Wonder', author: 'Yonas' },
-];
+const Books = () => {
+  const [books, useBooks] = useState([
+    { id: 1, title: 'Name the Tree', author: 'Seko' },
+    { id: 2, title: 'How We became Men', author: 'SekoViper' },
+    { id: 3, title: 'In the Wonder', author: 'Yonas' },
+  ]);
 
-const Books = () => (
-  <>
-    <BooksList books={books} />
-    <AddBook />
-  </>
-);
+  return (
+    <>
+      <BooksList books={books} />
+      <AddBook />
+    </>
+  );
+};
 
 export default Books;
