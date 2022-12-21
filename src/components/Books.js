0186eac77196784be09d439e91ce2c330ec3adb4
SekoVper <damehlivingstone1@gmail.com>
2022-12-21 14:54:20 +0100

    Use the useSelector to grab data from the store.

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,14 +1,10 @@
-import { React, useState } from 'react';
+import { React } from 'react';
+import { useSelector } from 'react-redux';
 import AddBook from './AddBook';
 import BooksList from './BooksList';
 
 const Books = () => {
-  const [books] = useState([
-    { id: 1, title: 'Name the Tree', author: 'Seko' },
-    { id: 2, title: 'How We became Menhhhhhh', author: 'SekoViper' },
-    { id: 3, title: 'In the Wonders', author: 'Yonas' },
-  ]);
-
+  const books = useSelector((state) => state.books);
   return (
     <>
       <BooksList books={books} />
