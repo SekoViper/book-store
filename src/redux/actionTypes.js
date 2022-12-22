95850a9db58e39cc937dce827cca30caa7afff48
SekoVper <damehlivingstone1@gmail.com>
2022-12-22 19:01:37 +0100

    Add get books action

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,3 +1,4 @@
 export const ADD_BOOK = 'book-store/books/ADD_BOOK';
 export const REMOVE_BOOK = 'book-store/books/REMOVE_BOOK';
 export const CHECK_STATUS = 'book-store/categories/CHECK_STATUS';
+export const GET_BOOKS = 'book-store/books/GET_BOOKS';
