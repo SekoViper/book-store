01e7772efd8fc95a5871223be7e4efb735470298
SekoVper <damehlivingstone1@gmail.com>
2022-12-22 19:04:03 +0100

    Refactor code to consume an API.

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,41 +1,53 @@
-import { v4 as uuidv4 } from 'uuid';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 import * as actions from '../actionTypes';
 
 // initial state
-const initialState = [
-  { id: uuidv4(), title: 'Whoa re you', author: 'Seko' },
-  { id: uuidv4(), title: 'Whoa re you', author: 'Seko' },
-  { id: uuidv4(), title: 'ghhhhhhhhhhhhghghgh', author: 'yuou' },
-];
+const initialState = [];
+const APIUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/NXqxKIJZ6HJNfUKJMR6J/books/';
 
 // reducer
 export default function booksReducer(state = initialState, action = {}) {
   switch (action.type) {
-    case actions.ADD_BOOK: return [
-      ...state, action.payload.book,
+    case `${actions.ADD_BOOK}/fulfilled`: return [
+      ...state, action.payload,
     ];
-    case actions.REMOVE_BOOK: return [
-      ...state.filter((book) => book.id !== action.payload.id),
+    case `${actions.REMOVE_BOOK}/fulfilled`: return [
+      ...state.filter((book) => book.item_id !== action.payload),
+    ];
+    case `${actions.GET_BOOKS}/fulfilled`: return [
+      ...state, ...action.payload,
     ];
     default: return state;
   }
 }
 
 // action creators
-export function removeBook(id) {
-  return {
-    type: actions.REMOVE_BOOK,
-    payload: {
-      id,
-    },
-  };
-}
+export const removeBook = createAsyncThunk(actions.REMOVE_BOOK, async (id) => {
+  await axios.delete(APIUrl + id);
+  return id;
+});
 
-export function addBook(book) {
-  return {
-    type: actions.ADD_BOOK,
-    payload: {
-      book,
-    },
-  };
-}
+export const addBook = createAsyncThunk(actions.ADD_BOOK, async (data) => {
+  const res = await axios.post(APIUrl, data);
+  if (res.data === 'Created') {
+    return data;
+  }
+  return null;
+});
+
+export const getBooks = createAsyncThunk(actions.GET_BOOKS, async () => {
+  const res = await axios
+    .get(APIUrl)
+    .then((data) => {
+      const books = Object.entries(data.data).map(([id, obj]) => ({
+        item_id: id,
+        ...obj[0],
+      }));
+      return books;
+    })
+    .catch((err) => {
+      console.log('Error', err);
+    });
+  return res;
+});
